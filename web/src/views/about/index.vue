0ab122b8c1a803b3782eb5dd032242d9429e9312
Curtion <curtion@126.com>
2022-03-03 17:56:32 +0800

    about

--- web/src/views/about/index.vue
+++ web/src/views/about/index.vue
@@ -1,1 +1,34 @@
-<template>DEMO</template>
+<template>
+  <div class="box">
+    <n-card title="关于" class="card">
+      <p>本软件免费开源，适合个人用户独立运行，不建议搭建公共平台。</p>
+      <p>
+        规则市场里面可以进行任务规则分享，这个功能正在开发中,不过不一定会上线。
+      </p>
+      <p>如果本软件对你有帮助，可以考虑进行捐赠，金额随意。</p>
+      <p class="alipay">
+        <img :src="alipay" alt="支付宝" width="240" height="360" />
+      </p>
+    </n-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NCard } from "naive-ui";
+import alipay from "@/assets/img/alipay.jpg";
+</script>
+<style scoped lang="scss">
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .card {
+    width: 600px;
+  }
+}
+.alipay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
