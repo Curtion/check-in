addb70cb6ad1095f7c52160e012e3c9fbcd05ec6
Curtion <curtion@126.com>
2022-03-16 22:04:35 +0800

    完成header组件

--- web/src/views/task/add/components/header.vue
+++ web/src/views/task/add/components/header.vue
@@ -1,35 +1,62 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { NSpace } from 'naive-ui'
-const data = defineProps({
-  data: {
-    type: Array,
-    default: () => [],
-  },
+const props = withDefaults(defineProps<{
+  data: Header[]
+}>(),
+{
+  data: () => [],
 })
-const url = ref('')
+
+interface Header {
+  key: string
+  value: string
+}
+
+const data = ref<Header[]>([])
+
+data.value.push(...props.data)
+
+if (data.value.length === 0) {
+  handleAddHeader()
+}
+
+function handleAddHeader() {
+  data.value.push({
+    key: '',
+    value: '',
+  })
+}
+
+function HandleDelHeader(index: number) {
+  data.value.splice(index, 1)
+}
+
+function handleClearHeader() {
+  data.value = []
+}
 </script>
 
 <template>
-  <n-grid class="mtb" :x-gap="12" :cols="12">
+  <n-grid v-for="(item,index) in data" :key="item.key" class="mtb" :x-gap="12" :cols="12">
     <n-gi :span="5">
-      <n-input v-model:value="url" placeholder="请输入Key" />
+      <n-input v-model:value="item.key" placeholder="请输入Key" />
     </n-gi>
     <n-gi :span="5">
-      <n-input v-model:value="url" placeholder="请输入Value" />
+      <n-input v-model:value="item.value" placeholder="请输入Value" />
     </n-gi>
     <n-gi :span="2">
-      <n-button type="error" attr-type="button">
+      <n-button type="error" attr-type="button" @click="HandleDelHeader(index)">
         删除
       </n-button>
     </n-gi>
   </n-grid>
   <n-space style="margin-top: 20px">
-    <n-button type="success" attr-type="button">
+    <n-button type="success" attr-type="button" @click="handleAddHeader">
       添加一行
     </n-button>
-    <n-button attr-type="button">
-      全部清空
+    <n-button attr-type="button" @click="handleClearHeader">
+      删除全部
     </n-button>
   </n-space>
 </template>
